c7a1af95dab9ecf9d617b5446d75bfe93cfe1ec1
AngelMagana214 <122948604+AngelMagana214@users.noreply.github.com>
2023-02-11 22:23:20 -0600

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,9 @@
+fun main() {
+        val totalSales = 4600000.0 //4.6 million in total sales.
+        val salesPercent = 0.62 // 62% of total sales
+        val result: Double
+        result = totalSales * salesPercent
+        println("When this company generates $$totalSales")
+        println("in sales this fiscal year, the East Coast")
+        println("division of this company will generate $$result")
+    }
