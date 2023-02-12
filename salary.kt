dc8fa39e69c311b2b46d046a9d7bddbdb6ca3a67
AngelMagana214 <122948604+AngelMagana214@users.noreply.github.com>
2023-02-11 20:57:33 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,6 @@
+    fun main() {
+        val name = "Angel Magana"
+        val age = 27
+        val annualPay = 1000000.0
+        println("My name is $name, my age is $age and I am going to earn $$annualPay per year.")
+    }
