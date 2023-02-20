d5f1b389728d4dfc7ed87bbfe59815fd5f497863
AngelMagana214 <122948604+AngelMagana214@users.noreply.github.com>
2023-02-19 21:31:49 -0600

    Create stringMani.kt

---
+++ stringMani.kt
@@ -0,0 +1,27 @@
+import java.util.*
+
+// Angel Magana Favorite City program
+fun main(args: Array<String>) {
+
+    
+    val keyboard = Scanner(System.`in`)
+
+    
+    print("Enter the name of your favorite city: ")
+    val city = keyboard.nextLine()
+
+
+    keyboard.close()
+
+   
+    println("Number of characters: " + city.length)
+
+   
+    println(city.uppercase(Locale.getDefault()))
+
+  
+    println(city.lowercase(Locale.getDefault()))
+
+    
+    println(city[0])
+}
